b89ad9b35d371778bb9daeba920d0a40eb86aa17
chefkasperson <chefkasperson@gmail.com>
2020-07-14 06:00:50 -0700

    added user click checker

--- game.js
+++ game.js
@@ -1,8 +1,19 @@
 const buttonColours = ['red', 'blue', 'green', 'yellow']
 const gamePattern = []
+const userClickedPattern = []
 
+$('btn').click(function() {
+    const userChosenColour = $(this).attr('id')
+    userClickedPattern.push(userChosenColour)
+})
 function nextSequence() {
     const randomNumber = Math.floor(Math.random() * 4)
     const randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour)
+    
+    $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+
+    const audio = new Audio('sounds/' + randomChosenColour + ".mp3")
+
+    audio.play()
 }
