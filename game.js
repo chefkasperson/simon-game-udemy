6ede1a0891f915f123e21285c8aa7724e7acb0d0
chefkasperson <chefkasperson@gmail.com>
2020-07-14 06:07:10 -0700

    added sound to user click

--- game.js
+++ game.js
@@ -5,15 +5,21 @@ const userClickedPattern = []
 $('btn').click(function() {
     const userChosenColour = $(this).attr('id')
     userClickedPattern.push(userChosenColour)
+    playSound(userChosenColour)
 })
+
 function nextSequence() {
     const randomNumber = Math.floor(Math.random() * 4)
     const randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour)
     
-    $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+    $('#' + name).fadeIn(100).fadeOut(100).fadeIn(100)
+    
+    playSound(randomChosenColour)
+}
 
-    const audio = new Audio('sounds/' + randomChosenColour + ".mp3")
+function playSound(name) {
+    const audio = new Audio('sounds/' + name + '.mp3')
 
     audio.play()
 }
